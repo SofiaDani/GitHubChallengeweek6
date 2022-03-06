32c3c211ebb9ef359ecf0ad54592d3be8cfb670d
SofiaDani <sofia.dsmachado@gmail.com>
2022-03-06 20:30:40 +0000

    Forecast API

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ if (minutes < 10) {
 } else {
   h3.innerHTML = `${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -57,7 +57,7 @@ function displayForecast() {
   <div class = "container-row-cols-md-3">
    <div class="card day">
     <div class="card-body">
-      <h5 class="card-title">THU</h5>
+      <h5 class="card-title">${day}</h5>
       <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42>
         <span class="card-text">14ºC</span>
         <span class="minimum-temp"> 12ºC</span>
@@ -71,6 +71,11 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "ab71c92f5b2dc5177b732d5b87167782";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function temperatureToday(response) {
   let iconElement = document.querySelector("#icon");
@@ -91,6 +96,8 @@ function temperatureToday(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 function searching(event) {
   event.preventDefault();
@@ -119,5 +126,3 @@ function fahrenheitToCelsius(event) {
 }
 let changeToCelsius = document.querySelector("#celsius-link");
 changeToCelsius.addEventListener("click", fahrenheitToCelsius);
-
-displayForecast();
